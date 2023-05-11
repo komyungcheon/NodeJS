4be2a78f76648529c410883e591b89c6b9f20d46
komyungcheon <shy06101997@gamil.com>
2023-05-11 11:38:08 +0700

    1

--- Tinhtrungbinhtongbinhphuong/app.js
+++ Tinhtrungbinhtongbinhphuong/app.js
@@ -1,9 +1,9 @@
-let number = [1,2,3,4,5,6,7,8,9,10]
+let number = [1,2,3,4,5,7,9,]
 // let cacsochan = number.filter((item)=> item % 2 === 0)
 // console.log(cacsochan);
 // let sum = cacsochan.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 // console.log(sum)
-
+//
 let total = number.reduce(function (total,currentValue) {
     if(currentValue % 2 === 0){
         return total + currentValue
@@ -12,6 +12,13 @@ let total = number.reduce(function (total,currentValue) {
 },0);
 console.log(total)
 
-let trungbinhtongbinhphuong = Math.pow(total,2)
-console.log(trungbinhtongbinhphuong)
+// let trungbinhtongbinhphuong = Math.pow(total,2)
+// console.log(trungbinhtongbinhphuong)
+let trungbinhtongbinhphuong = number.reduce((function (total,currentValue){
+    if(currentValue % 2 === 0){
+        return total + Math.pow(currentValue,2)
+    } else return total
+}),0)
+console.log(trungbinhtongbinhphuong);
+console.log(Math.sqrt(trungbinhtongbinhphuong))
 
